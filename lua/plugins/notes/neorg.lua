cfb90c0306b918584855f41e3565432d02797b00
Arcemist <niverdanielarce@gmail.com>
2024-08-19 04:35:10 -0300

    Mejore el coso de las notas

--- lua/plugins/notes/neorg.lua
+++ lua/plugins/notes/neorg.lua
@@ -1,7 +1,7 @@
 return {
-  "nvim-neorg/neorg",
+  'nvim-neorg/neorg',
   lazy = false,
-  version = "*",
+  version = '*',
   config = function()
   require("neorg").setup {
     load = {
@@ -20,20 +20,49 @@ return {
           default_workspace = "notes",
         },
       },
+      ["core.completion"] = {
+        config = {
+          engine = "nvim-cmp"
+        }
+      },
+      ["core.integrations.telescope"] = {},
+      ["core.integrations.treesitter"] = {
+        config = {
+          install_parsers = true,
+          configure_parsers = true
+        }
+      },
     },
   }
   vim.wo.foldlevel = 99
   vim.wo.conceallevel = 2
   end,
+
+  dependencies = {
+    {'nvim-lua/plenary.nvim'},
+    {'nvim-neorg/neorg-telescope'}
+  },
 },
 
-vim.keymap.set('n', '<leader>ng' ,':Neorg workspace notes\n', {
+
+vim.keymap.set('n', '<CR>', '<Plug>(neorg.esupports.hop.hop-link)', {
+  desc = 'Go to link'
+}),
+vim.keymap.set('n', '<leader>n<leader>' ,':Neorg workspace notes\n', {
   desc = 'Go to my notes'
 }),
 vim.keymap.set('n', '<leader>nn', '<Plug>(neorg.dirman.new-note)', {
   desc = 'Create a new note'
 }),
-
+vim.keymap.set('n', '<leader>nf', '<Plug>(neorg.telescope.find_norg_files)', {
+  desc = 'Search Norg files'
+}),
+vim.keymap.set('n', '<leader>ng', '<Plug>(neorg.telescope.find_linkable)', {
+  desc = 'Search linkables'
+}),
+vim.keymap.set('n', '<leader>nl', '<Plug>(neorg.telescope.insert_link)', {
+  desc = 'Insert a link'
+}),
 vim.keymap.set('n', '<C-Space>', '<Plug>(neorg.qol.todo-items.todo.task-cycle)', {
   desc = 'Clicle the states of a task',
 }),
